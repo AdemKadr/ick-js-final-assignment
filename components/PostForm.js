1befbf563d4428994134a829ef03b3f7eaf5b4fc
Adem Kadriu <adem-kadriu@hotmail.com>
2020-11-29 15:20:28 +0100

    added new files.

---
+++ components/PostForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class UserForm extends React.Component {
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit}>
+                <h2>Create a post</h2>
+                <div class = "first">
+                     <label for="title">Title</label>
+                    <input type="text" placeholder="Enter title..." value=""required />
+                </div>
+                <div>
+                    <label for="contetnt">Content</label>
+                    <input type="textarea" placeholder="Enter Content..."  value=""required />
+                </div>
+
+                <button type='submit'>Add</button>
+
+            </form>
+        );
+    }
+}
+
+export default UserForm;
