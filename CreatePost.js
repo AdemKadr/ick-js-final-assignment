b6b75d8db3a2d3ac6b74607c9b7dfa7589bf6e8a
Adem Kadriu <adem-kadriu@hotmail.com>
2020-11-29 20:23:38 +0100

    Hope this will work

--- CreatePost.js
+++ CreatePost.js
@@ -2,23 +2,76 @@ import React from 'react';
 import FormPost from './FormPost';
 import axios from 'axios';
 
-class CreatPost extends React.Component{
-constructor(){
-    super();
-    this.state ={
-        isLoading: true
+class CreatePost extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            isLoading: true
+            
+        }
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+
+    componentDidMount(){
+        axios
+            .get(form.title,form.content)
+            .then(res =>{
+                this.setState({
+                    title:res.data,
+                    isLoading:false
+                });
+            })
+            .catch(e =>{
+                this.setState({
+                    isLoading:false
+                });
+                alert("Couldn't load text.")
+            });
+    }
+
+    handleChange(event){
+        const title = event.target.title;
+
+        this.setState(prevState =>{
+            return {
+                form: {
+                    ...prevState.form,
+                    [title]: event.target.value
+                }
+            };
+        });
+    }
+
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        this.setState(prevState =>{
+            return{
+                form: {
+                    title: '',
+                    content: ''
+                }
+            };
+        });
+    }
+
+    render(){
+        if (this.state.isLoading){
+            return <h2>Loading</h2>
+        }
+
+        return(
+            <div>
+                <FormPost
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                    form={this.state.form}
+                />
+            </div>
+        );
     }
 }
-
-
-componentDidMount(){
-
-}
-
-
-
-
-
-
-
-}
+export default CreatePost;
